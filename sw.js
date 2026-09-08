12edcbd5c281c3b075d86f33bc068107c6f0f5b7
Mohammad Rahimi <mohdm2269@gmail.com>
2026-09-08 19:03:14 +0400

    Update CACHE_VERSION to mss-v244

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache strategy: stale-while-revalidate
 ══════════════════════════════════════════ */
 
-const CACHE_VERSION = 'mss-v237';
+const CACHE_VERSION = 'mss-v244';
 const STATIC_CACHE  = `${CACHE_VERSION}-static`;
 const IMAGE_CACHE   = `${CACHE_VERSION}-images`;
 
