91c3ee0b8947d59acbb4a525e7116f78beb92865
Mohammad Rahimi <mohdm2269@gmail.com>
2026-09-13 20:16:20 +0400

    Update CACHE_VERSION in service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache strategy: stale-while-revalidate
 ══════════════════════════════════════════ */
 
-const CACHE_VERSION = 'mss-v260';
+const CACHE_VERSION = 'mss-v376';
 const STATIC_CACHE  = `${CACHE_VERSION}-static`;
 const IMAGE_CACHE   = `${CACHE_VERSION}-images`;
 
