e8bed0a6aa3a473f247622262ea9387fd97c76ad
Mohammad Rahimi <mohdm2269@gmail.com>
2026-09-24 12:13:19 +0400

    Update cache version to mss-v555

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache strategy: stale-while-revalidate
 ══════════════════════════════════════════ */
 
-const CACHE_VERSION = 'mss-v554';
+const CACHE_VERSION = 'mss-v555';
 const STATIC_CACHE  = `${CACHE_VERSION}-static`;
 const IMAGE_CACHE   = `${CACHE_VERSION}-images`;
 
